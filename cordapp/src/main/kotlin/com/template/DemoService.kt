83e1ca203604109efc11532398bebfee5b2808f3
Fuzz <fuzz@bluebank.io>
2018-08-03 09:50:57 +0100

    bootstrap insurance service

--- cordapp/src/main/kotlin/com/template/DemoService.kt
+++ cordapp/src/main/kotlin/com/template/DemoService.kt
@@ -1,5 +1,7 @@
 package com.template
 
+import com.template.insurer.InsurerAPI
+import io.bluebank.braid.corda.BraidConfig
 import net.corda.core.node.AppServiceHub
 import net.corda.core.node.NodeInfo
 import net.corda.core.node.services.CordaService
@@ -7,7 +9,7 @@ import net.corda.core.serialization.SingletonSerializeAsToken
 import net.corda.core.utilities.loggerFor
 
 @CordaService
-class DemoService(serviceHub: AppServiceHub) : SingletonSerializeAsToken() {
+class DemoService(private val serviceHub: AppServiceHub) : SingletonSerializeAsToken() {
     companion object {
         private val log = loggerFor<DemoService>()
     }
@@ -16,15 +18,11 @@ class DemoService(serviceHub: AppServiceHub) : SingletonSerializeAsToken() {
         serviceHub.myInfo.apply {
             when {
                 this.isOfNodeType("insurer") -> configureInsurer()
-                this.isOfNodeType("bank") -> configureBank()
-                this.isOfNodeType("hospital") -> configureHospital()
+//                this.isOfNodeType("bank") -> configureBank()
+//                this.isOfNodeType("hospital") -> configureHospital()
                 else -> configureOtherNode()
             }
         }
-    }
-
-    private fun configureOtherNode() {
-
     }
 
     private fun configureHospital() {
@@ -36,10 +34,18 @@ class DemoService(serviceHub: AppServiceHub) : SingletonSerializeAsToken() {
     }
 
     private fun configureInsurer() {
-
+        val service = InsurerAPI(serviceHub)
+        BraidConfig()
+            .withPort(8001)
+            .withService("insurer", service)
+            .bootstrapBraid(serviceHub)
     }
 
     private fun NodeInfo.isOfNodeType(name: String): Boolean {
         return legalIdentities.any { it.name.organisation.contains(name, true) }
+    }
+
+    private fun configureOtherNode() {
+        log.info("unknown node type. not configuring any services")
     }
 }
