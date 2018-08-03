e295bdaa72039b2171b009ffdd6fbed847557bcd
Fuzz <fuzz@bluebank.io>
2018-08-03 09:23:33 +0100

    basic structure for demo web service configuration

---
+++ cordapp/src/main/kotlin/com/template/DemoService.kt
@@ -0,0 +1,45 @@
+package com.template
+
+import net.corda.core.node.AppServiceHub
+import net.corda.core.node.NodeInfo
+import net.corda.core.node.services.CordaService
+import net.corda.core.serialization.SingletonSerializeAsToken
+import net.corda.core.utilities.loggerFor
+
+@CordaService
+class DemoService(serviceHub: AppServiceHub) : SingletonSerializeAsToken() {
+  companion object {
+    private val log = loggerFor<DemoService>()
+  }
+
+  init {
+    serviceHub.myInfo.apply {
+      when {
+        this.isOfNodeType("insurer") -> configureInsurer()
+        this.isOfNodeType("bank") -> configureBank()
+        this.isOfNodeType("hospital") -> configureHospital()
+        else -> configureOtherNode()
+      }
+    }
+  }
+
+  private fun configureOtherNode() {
+
+  }
+
+  private fun configureHospital() {
+
+  }
+
+  private fun configureBank() {
+
+  }
+
+  private fun configureInsurer() {
+
+  }
+
+  private fun NodeInfo.isOfNodeType(name: String) : Boolean {
+    return legalIdentities.any { it.name.organisation.contains(name, true) }
+  }
+}
---
+++ cordapp/src/main/kotlin/com/template/insurer/InsurerAPI.kt
@@ -0,0 +1,10 @@
+package com.template.insurer
+
+import net.corda.core.node.AppServiceHub
+
+class InsurerAPI(private val serviceHub: AppServiceHub){
+  fun initialiseDemo() {
+
+  }
+}
+
