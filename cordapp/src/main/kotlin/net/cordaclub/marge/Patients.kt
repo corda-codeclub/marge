b745c9036ff89b292be681b6121770ade4adc966
Fuzz <fuzz@bluebank.io>
2018-08-04 21:17:59 +0100

    some names

--- cordapp/src/main/kotlin/net/cordaclub/marge/Patients.kt
+++ cordapp/src/main/kotlin/net/cordaclub/marge/Patients.kt
@@ -2,10 +2,11 @@ package net.cordaclub.marge
 
 object Patients {
     // todo - come up with better patients
+    // Tudor - how about a selection from https://github.com/moby/moby/blob/master/pkg/namesgenerator/names-generator.go
     val allPatients = listOf(
-            Patient("patient1", "ab123456b"),
-            Patient("patient2", "ab123456c"),
-            Patient("patient3", "ab123456d"),
-            Patient("patient4", "ab123456e")
+            Patient("Joan Clarke", "ab123456b"),
+            Patient("Seymour Roger Cray", "ab123456c"),
+            Patient("Dorothy Vaughan", "ab123456d"),
+            Patient("Steve Wozniak", "ab123456e")
     )
 }
